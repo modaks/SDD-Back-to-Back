3d10c7f94119af357e9a7b8576eca8c44bae2847
modaks <samimodak@gmail.com>
2015-11-03 18:43:46 -0500

    created function for pulliong clothing and setting up into objects

--- app/src/main/java/com/example/root/myapplication/MainActivity.java
+++ app/src/main/java/com/example/root/myapplication/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Checks if coming from recently liked page or swiping page
     private boolean fromSwiping = true;
-
+    private ArrayList<Clothing> arrayClothingOutput;
     // Create array of clothing objects
     private ArrayList<Clothing> arrayClothing;
 
@@ -62,39 +62,10 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
+        arrayClothingOutput = new ArrayList<Clothing>();
+
         // Create clothing objects and clothing arrays
-        arrayClothing = new ArrayList<Clothing>();
-
-        //create new thread since we are accessing Network
-        new Thread(){
-            public void run() {
-
-                //string will contain allJson text
-                String link="";
-
-                try{
-                    //retreiving json info regarding all clothes
-                    link = getHTML("http://ec2-54-210-37-207.compute-1.amazonaws.com/getProducts/AkshayMata");
-                }catch(Exception e){
-                    Log.i(LOG_TAG,"Failed to retrieve Jsoin string from onCreate");
-                }
-
-                //Tokens tores individual Json objaects as strings
-                String[] tokens = link.split("\\}\\{");
-                //Tokens_content temporarily stores content of each individual token
-                String[] tokens_content=null;
-                //Delims will be used to parse tokens into individual content
-                String delims1 = "u'merchant': u'|\\', u'category': |\\, u'description': u\"|\\\", u'url': u'|\\', u'image': \\[u'|\\'\\], u'on_sale': |\\, u'price': |\\, u'part_number': u'|\\', u'_id': ObjectId\\('|\\'\\), u'brand': u'|\\', u'name': u|\\, u'description': u'|\\', u'url': u'";
-
-                for (int i=0; i<tokens.length;i++){
-                    tokens_content = tokens[i].split(delims1);
-                    if(tokens_content.length==12){
-                        arrayClothing.add(new Clothing(tokens_content[1],tokens_content[2],tokens_content[3],tokens_content[4],tokens_content[5],tokens_content[6],tokens_content[7],tokens_content[8],tokens_content[9],tokens_content[10],tokens_content[11]));
-
-                    }
-                }
-            }
-        }.start();
+       arrayClothing=getClothing("http://ec2-54-210-37-207.compute-1.amazonaws.com/getProducts/AkshayMata");
 
         // Set current bitmap image
         bitmap = getBitmapFromURL("http://loololi.com/wp-content/uploads/2013/09/Scarves-175-1.jpg");
@@ -116,6 +87,44 @@ public class MainActivity extends AppCompatActivity {
         //Initialize liked clothing array
         arrayLikedClothing = new ArrayList<Clothing>();
 
+    }
+
+    public ArrayList<Clothing> getClothing(final String urlAWS){
+
+        arrayClothingOutput.clear();
+
+        //create new thread since we are accessing Network
+        new Thread(){
+            public void run() {
+
+                //string will contain allJson text
+                String link="";
+
+                try{
+                    //retreiving json info regarding all clothes
+                    link = getHTML(urlAWS);
+                }catch(Exception e){
+                    Log.i(LOG_TAG,"Failed to retrieve Jsoin string from onCreate");
+                }
+
+                //Tokens tores individual Json objaects as strings
+                String[] tokens = link.split("\\}\\{");
+                //Tokens_content temporarily stores content of each individual token
+                String[] tokens_content=null;
+                //Delims will be used to parse tokens into individual content
+                String delims1 = "u'merchant': u'|\\', u'category': |\\, u'description': u\"|\\\", u'url': u'|\\', u'image': \\[u'|\\'\\], u'on_sale': |\\, u'price': |\\, u'part_number': u'|\\', u'_id': ObjectId\\('|\\'\\), u'brand': u'|\\', u'name': u|\\, u'description': u'|\\', u'url': u'";
+
+                for (int i=0; i<tokens.length;i++){
+                    tokens_content = tokens[i].split(delims1);
+                    if(tokens_content.length==12){
+                        arrayClothingOutput.add(new Clothing(tokens_content[1],tokens_content[2],tokens_content[3],tokens_content[4],tokens_content[5],tokens_content[6],tokens_content[7],tokens_content[8],tokens_content[9],tokens_content[10],tokens_content[11]));
+
+                    }
+                }
+            }
+        }.start();
+
+        return arrayClothingOutput;
     }
     public static String getHTML(String urlToRead) throws Exception {
         try {
